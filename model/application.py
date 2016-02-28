91cda0c614f4dc820d48af3ca43b9247894fa50f
msarana <maryia.sarana@gmail.com>
2016-02-27 16:37:14 -0800

    Putting helper methods in the applications class

--- model/application.py
+++ model/application.py
@@ -9,4 +9,20 @@ class Application(object):
 
     def __init__(self, driver):
         self.driver = driver
-        
+    
+    def go_to_home_page(self):
+        self.driver.get("http://localhost/php4dvd/")
+    
+    def login(self, user):
+        driver = self.driver
+        driver.find_element_by_id("username").clear()
+        driver.find_element_by_id("username").send_keys(user.username)
+        driver.find_element_by_name("password").clear()
+        driver.find_element_by_name("password").send_keys(user.password)
+        driver.find_element_by_name("submit").click()
+    
+def logout(self):
+        driver = self.driver
+        driver.find_element_by_link_text("Log out").click()
+        driver.switch_to_alert().accept()
+    
--- test_login.py
+++ test_login.py
@@ -12,20 +12,10 @@ from model.user import User
 
 
 def test_login(app):
-    app.driver.get("http:localhost/php4dvd/")
-    login(app.driver, User.Admin())
-    logout(app.driver)
+    app.go_to_home_page()
+    app.login(User.Admin())
+    app.logout()
 
-def login(driver, user):
-    driver.find_element_by_id("username").clear()
-    driver.find_element_by_id("username").send_keys(user.username)
-    driver.find_element_by_name("password").clear()
-    driver.find_element_by_name("password").send_keys(user.password)
-    driver.find_element_by_name("submit").click()
-    
-def logout(driver):
-    driver.find_element_by_link_text("Log out").click()
-    driver.switch_to_alert().accept()
    
     
 
